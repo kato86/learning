f690d61a729e50cabd59ce91cd16f8620277f63d
Kamil <tomala.kamil@gmail.com>
2021-07-15 12:24:36 +0200

    Create array.js file
    Create an array in a variety of ways.

---
+++ array.js
@@ -0,0 +1,16 @@
+// Creating arrays
+// 1
+const arr = [1, 2, 3];
+console.log(arr);
+// 2
+const arr2 = new Array(1, 2, 3);
+console.log(arr2);
+// 3
+const arr3 = Array(1, 2, 3);
+console.log(arr3);
+// 4
+const arr4 = Array.of(1, 2, 3);
+console.log(arr4);
+// 5
+const arr5 = Array.from('Some string');
+console.log(arr5);
