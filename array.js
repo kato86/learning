cc0efe16aca224d7a043c4eab1e11c8a346bafcb
Kamil <tomala.kamil@hotmail.com>
2021-07-15 12:56:05 +0200

    feat(add data): add data to array

--- array.js
+++ array.js
@@ -1,4 +1,6 @@
+// **********************************************
 // Creating arrays
+
 // 1
 const arr = [1, 2, 3];
 console.log(arr);
@@ -15,7 +17,9 @@ console.log(arr4);
 const arr5 = Array.from('Some string');
 console.log(arr5);
 
+// **********************************************
 // NESTED ARRAYS
+
 const nestArr = [[1, 2, 3], [4, 5, 6]];
 
 for (let data of nestArr) {
@@ -23,3 +27,20 @@ for (let data of nestArr) {
         console.log(dataPoint);
     }
 }
+
+// **********************************************
+// ADD DATA TO ARRAY
+
+const hobbies = ['Sports', 'Reading'];
+// add at the end of the array
+hobbies.push('Cooking');
+console.log(hobbies);
+// add at the beginning of the array
+hobbies.unshift('Running');
+console.log(hobbies);
+// remove element at the beginning of the array
+hobbies.shift();
+console.log(hobbies);
+// remove element at the end of the array
+hobbies.pop();
+console.log(hobbies);
